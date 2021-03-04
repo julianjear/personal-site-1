daab8530b4fdedeb3c9b699f538aca54cd879fb0
Julian Alvarez <julianjear10@gmail.com>
2021-03-03 22:02:31 -0600

    Added ITF to home page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -24,25 +24,92 @@ const Index = () => (
         <Link to="/resume">resume</Link> {' '}
         or <Link to="/contact">contact me</Link>.
       </p>
-      <h3>The Launch of My New Podcast, Inventing The Future</h3>
+      <h2>Inventing The Future Podcast</h2>
+      <a
+        href="https://inventingthefuture.ai/"
+        target="_blank"
+        rel="noreferrer"
+        style={{ textDecoration: "none", borderBottomStyle: "none" }}
+      >
+        <img src="images/Inventing The Future_Cover_Space.png" alt="Inventing The Future Logo" width="60%" />
+      </a>
       <p>
-        Inventing The Future is a podcast that I will be launching on the week of Jan 11th, 2021.
-        This podcast will focus on the meaningful problems that entrepreneurs around the world {' '}
-        are working to solve, how they are approaching the problem with their unique solution,
+        Inventing The Future is an entrepreneurship podcast with the mission of inspiring and {' '}
+        empowering entrepreneurs to solve the world's biggest problems. {' '}
+        This podcast focuses on exploring the most significant problems that entrepreneurs {' '}
+        around the world are working to solve, how they are approaching the problem with their {' '}
+        unique solution, the entrepreneurial lessons they have learned along the way,
         and the mindsets that they have developed to endure the adversity and challenges of {' '}
         building something from the ground up. If you are interested in entrepreneurship,
         business, leadership, exponential technologies, or self-improvement, you'll love {' '}
         this podcast.
       </p>
+      <button type="button">
+        <a
+          href="https://inventingthefuture.ai/"
+          target="_blank"
+          rel="noreferrer"
+          style={{ textDecoration: "none", borderBottomStyle: "none", fontSize: "12px" }}
+        >
+          Listen Now!
+        </a>
+      </button>
+      <br />
+      <p>
+        <br />
+        <strong>Here is the show description of the podcast:</strong>
+        <br />
+        The potential for entrepreneurs to disrupt industries and solve problems on a massive  {' '}
+        scale has never been greater. The decreased cost and increased power of technology has  {' '}
+        democratized our ability to solve large-scale problems. Today, a single individual or team,
+        driven by a purpose, can transform our world. {' '}
+        Our mission is to inspire and empower entrepreneurs to solve the world's biggest problems. {' '}
+        Join me, a growing entrepreneur and computer scientist, as I interview those that are  {' '}
+        inventing our future. The best way to predict the future is to invent it. {' '}
+        <br />
+        How will you invent the future? {' '}
+      </p>
+
+      <h3>Inventing The Future Newsletter</h3>
       <p>
         Subscribe to my newsletter to recieve updates on new episodes every 2 weeks. I'll also {' '}
-        include insights, resources, and quotes that are designed to help you live a healthier,
-        fulfilling, and more productive life. If you sign up right now, you'll receive an {' '}
-        <a href="https://www.linkedin.com/feed/update/urn:li:activity:6751186976025706496/" target="_blank" rel="noreferrer">annual review and reflection Notion template</a> {' '}
-        to help you win your year.
+        share notes, insights, wisdom, tools, and resources that are designed to help you become {' '}
+        a better entrepreneur and live a healthier, fulfilling, and more productive life. {' '}
       </p>
       <h4>Subscribe</h4>
       <MailchimpSubscribe url={url} />
+      <br />
+      <h3>Podcast Slack Community</h3>
+      <p>
+        <strong>There are two primary reasons why I created a Slack community for this podcast.</strong> {' '}
+        <br />
+        1. Podcasts often discuss really interesting topics but the conversations are usually one {' '}
+        directional, which means that they go from the host/guests to the listener. In this {' '}
+        community, we'll keep the conversation from the topics discussed in the podcast going. This means {' '}
+        you'll be able to have conversations with the host (me) and other listeners related to the {' '}
+        topics discussed in the episodes. {' '}
+        <br />
+        2. If you really enjoy a podcast, it is likely that you would relate well with other listeners {' '}
+        that also enjoy the podcast. This podcast community creates a space where you can meet,
+        engage with, learn from, and potentially collaborate with other like-minded entrepreneurs. {' '}
+        <br />
+        <br />
+        If you are passionate about entrepreneurship and would like to learn from and connect with other {' '}
+        ambitious entrepreneurs, I invite you to join the community and share anything related to {' '}
+        entrepreneurship that you think would be valuable to others. {' '}
+        <br />
+        Let's invent the future, together.
+      </p>
+      <button type="button">
+        <a
+          href="https://inventingthefuturepod.slack.com/join/shared_invite/zt-msqn2ldi-3gPROHj8xPXXuvocj0vufw#/"
+          target="_blank"
+          rel="noreferrer"
+          style={{ textDecoration: "none", borderBottomStyle: "none", fontSize: "12px" }}
+        >
+          Join The Slack Community
+        </a>
+      </button>
     </article>
   </Main>
 );
